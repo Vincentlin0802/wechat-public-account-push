69830f103f0e8a734f81a3be18ed6707484a9c8f
Vincent <46735163+Vincentlin0802@users.noreply.github.com>
2023-05-18 16:18:07 +0100

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -49,7 +49,7 @@ const USER_CONFIG = {
 
   CALLBACK_USERS: [
     {
-      name: 'Vincent Lin',
+      name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'owXJW6FczIJuecfZxvQDtk2ydR58',
     }
